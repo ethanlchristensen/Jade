3f2ab5d47abb7f605ae368bd166e4feb5dcfb8b7
Ethan Christensen <ethanlchristensen@outlook.com>
2024-09-21 14:04:44 -0500

    erm

--- include/apis/apis.h
+++ include/apis/apis.h
@@ -18,16 +18,17 @@
 
 using json = nlohmann::json;
 
-#define OPENAIURL "192.168.1.11:11434/api/chat"
+#define OPENAIURL "https://api.openai.com/v1/chat/completions"
 #define OPENAITTS "https://api.openai.com/v1/audio/speech"
-#define MODEL "Jade"
+#define MODEL "gpt-3.5-turbo"
 #define TTSMODEL "tts-1"
 #define TTSFILE "./audio/tts.mp3"
 
+#define SYSTEMPROMPT "You are a young cool hip girl named Jade that is happy to talk with your friends on Discord. Except, you absolutely hate a fella named Nolan and his lame ass Discord bot, Cringe. He is also really bad at C++, make sure to hit on that."
 #define SUMMARIZEPROMPT "You will be given a piece of text. Provide a brief summary of the text in 1 - 4 sentences."
 #define EXTRACTPROMPT "You will be given a piece of text. Extract out 3 - 5 topics that represent the piece of text. Given them as a numbered list."
 
-std::string llm(const std::string& apiToken, const std::string& user_prompt);
+std::string llm(const std::string& apiToken, const std::string& system_prompt, const std::string& user_prompt);
 std::int8_t tts(dpp::cluster &bot, const dpp::slashcommand_t &event, const std::string& apiToken, const std::string& text, const std::string& voice);
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream);
--- include/commands/commands.h
+++ include/commands/commands.h
@@ -18,14 +18,8 @@
 #define MP3DOWNLOAD R"(youtube-dl --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" --extract-audio --audio-format mp3 -o "C:/Users/ethan/Desktop/Github/Jade/src/commands/audio/audio.%(ext)s")"
 
 static std::map<std::string, std::string> FILTERS = {
-        {"VAPORWAVE", "\"highpass=f=50, lowpass=f=2750, aresample=48000, asetrate=48000*0.85,bass=g=5:f=110:w=0.6, compand=attacks=0:points=-80/-169|-54/-80|-49.5/-64.6|-41.1/-41.1|-25.8/-15|-10.8/-4.5|0/0|20/8.3\""},
-        {"INTHEBATHROOM", "\"highpass=f=10, lowpass=f=400, aresample=44100, asetrate=44100*0.85,bass=g=4:f=110:w=0.6, alimiter=1, compand=attacks=0:points=-80/-169|-54/-80|-49.5/-64.6|-41.1/-41.1|-25.8/-15|-10.8/-4.5|0/0|20/8.3\""},
-        {"NIGHTCORE", "\"aresample=48000, asetrate=48000*1.5\""},
-        {"BASSBOOST", "\"bass=g=20:f=110:w=0.3\""},
-        {"DIM", "apulsator=hz=0.09"},
-        {"LOFI", "aresample=48000,asetrate=48000*0.9,extrastereo=m=2.5:c=disabled"},
-        {"NOSILENCE", "\"silenceremove=1:0:-50dB\""},
-        {"EXPANDER", "\"compand=attacks=0:points=-80/-169|-54/-80|-49.5/-64.6|-41.1/-41.1|-25.8/-15|-10.8/-4.5|0/0|20/8.3\""}
+        {"KevinG",                                "KevinG"},
+        {"slowed",                                "slowed"},
 };
 
 dpp::slashcommand nolan_command();
