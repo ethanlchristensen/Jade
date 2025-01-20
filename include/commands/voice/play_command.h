835009291304f597fd5c0797edf75e1ba4101334
ethanlchristensen <ethanlchristensen@outlook.com>
2025-01-20 14:36:19 -0600

    [update] - fix bass boosted section

--- include/commands/voice/play_command.h
+++ include/commands/voice/play_command.h
@@ -12,7 +12,7 @@ static std::map<std::string, std::string> FILTERS = {
         {"Vaporwave", "\"highpass=f=50, lowpass=f=2750, aresample=48000, asetrate=48000*0.85,bass=g=5:f=110:w=0.6, compand=attacks=0:points=-80/-169|-54/-80|-49.5/-64.6|-41.1/-41.1|-25.8/-15|-10.8/-4.5|0/0|20/8.3\""},
         {"Klim in the bathroom in the apartment next door", "\"highpass=f=10, lowpass=f=400, aresample=44100, asetrate=44100*0.85,bass=g=4:f=110:w=0.6, alimiter=1, compand=attacks=0:points=-80/-169|-54/-80|-49.5/-64.6|-41.1/-41.1|-25.8/-15|-10.8/-4.5|0/0|20/8.3\""},
         {"Nightcore (sped up)", "\"aresample=48000, asetrate=48000*1.5\""},
-        {"Bass Boosted", "\"bass=g=10:f=110:w=0.3\""},
+        {"Bass Boosted", "bass=g=9:f=160:w=0.8"},
         {"Circling", "apulsator=hz=0.09"},
         {"Lofi", "aresample=48000,asetrate=48000*0.9,extrastereo=m=2.5:c=disabled"},
         {"Silence Removed", "\"silenceremove=1:0:-50dB\""},
